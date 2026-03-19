9a95607c3ecb1ee06fe45375c7f07bfca4d54b18
Connor Shore <cjshore12@gmail.com>
2026-03-19 16:36:15 -0600

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "ImGuizmo"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++23"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "ImGuizmo.h",
+        "ImGuizmo.cpp"
+    }
+
+    includedirs {
+        ".",
+        -- CRITICAL: ImGuizmo needs to find imgui.h and imgui_internal.h
+        -- Adjust this path to point to wherever your ImGui submodule lives
+        -- relative to your workspace root!
+        "%{wks.location}/Ember/vendor/imgui" 
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "EB_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "EB_RELEASE"
+        runtime "Release"
+        optimize "on"
